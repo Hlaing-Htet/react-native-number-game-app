4851eed15599652ca0b0cc2720e5a326be5a5923
Hlaing Htet <99160494+Hlaing-Htet@users.noreply.github.com>
2023-07-24 22:10:00 +0630

    start game screen

--- App.js
+++ App.js
@@ -2,7 +2,19 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, ImageBackground } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
+import GameScreen from "./screens/GameScreen";
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
+  }
+  let sereen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+
+  if (userNumber) {
+    sereen = <GameScreen />;
+  }
   return (
     <LinearGradient colors={["#4e163c", "#ddb52f"]} style={styles.rootScreen}>
       <ImageBackground
@@ -11,7 +23,7 @@ export default function App() {
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
-        <StartGameScreen />
+        {sereen}
       </ImageBackground>
     </LinearGradient>
   );
