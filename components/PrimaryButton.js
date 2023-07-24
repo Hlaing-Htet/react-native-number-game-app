2664e92b4df2f946af86783177617fe675b1c73b
Hlaing Htet <99160494+Hlaing-Htet@users.noreply.github.com>
2023-07-24 11:11:42 +0630

    alert

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View, Pressable } from "react-native";
 import React from "react";
 
-const PrimaryButton = ({ children }) => {
-  function pressHandler() {
-    console.log("Pessed!");
-  }
+const PrimaryButton = ({ children, onPress }) => {
+  // function pressHandler() {
+  //   console.log("Pessed!");
+  // }
   return (
     <View style={styles.buttonOuterContainer}>
       <Pressable
@@ -13,7 +13,7 @@ const PrimaryButton = ({ children }) => {
             ? [styles.buttonInnerContainer, styles.pressed]
             : styles.buttonInnerContainer
         }
-        onPress={pressHandler}
+        onPress={onPress}
         android_ripple={{ color: "#640233" }}
       >
         <Text style={styles.buttonText}>{children}</Text>
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,8 +1,28 @@
-import { StyleSheet, Text, View, TextInput } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, TextInput, Alert } from "react-native";
+import React, { useState } from "react";
 import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
+  const [enteredNumber, setEnteredNumber] = useState("");
+
+  function numberInputHandler(enterText) {
+    setEnteredNumber(enterText);
+  }
+  function resetInputHandler() {
+    setEnteredNumber("");
+  }
+  function confirmInputHandler() {
+    const chosenNumber = parseInt(enteredNumber);
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      //show alert
+      Alert.alert(
+        "Invalid Number!",
+        "Number has to be a number between 1 and 99",
+        [{ text: "Okey", style: "destructive", onPress: resetInputHandler }]
+      );
+      return;
+    }
+  }
   return (
     <View style={styles.inputContainer}>
       <TextInput
@@ -11,13 +31,15 @@ const StartGameScreen = () => {
         keyboardType="number-pad"
         autoCapitalize="none"
         autoCorrect={false}
+        value={enteredNumber}
+        onChangeText={numberInputHandler}
       />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Reset</PrimaryButton>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
         </View>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Confirm</PrimaryButton>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
         </View>
       </View>
     </View>
